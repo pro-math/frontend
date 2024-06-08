13dbd026cc6408285a421d232dd2a3dba45e7d93
ShinyTrunk <maksim19032007@gmail.com>
2024-06-08 13:03:29 +0300

    clean main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import './assets/main.css';
 import piniaPluginPersistedState from "pinia-plugin-persistedstate"
-// import 'vuetify/styles'
-// import { createVuetify } from 'vuetify'
-// import * as components from 'vuetify/components'
-// import * as directives from 'vuetify/directives'
-// import 'animate.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -12,16 +7,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// const vuetify = createVuetify({
-//     components,
-//     directives,
-//   })
-
 const app = createApp(App)
 
 app.use(createPinia().use(piniaPluginPersistedState))
 app.use(router)
-// app.use(vuetify)
 
 app.mount('#app')
 
