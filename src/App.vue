4c7bfe733e6d7979efc510ed5975d6ca5b8f2555
ShinyTrunk <maksim19032007@gmail.com>
2024-06-08 13:04:39 +0300

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
-import { RouterView } from 'vue-router';
-import AppFooter from './components/footer/AppFooter.vue';
-import AppHeader from './components/header_components/AppHeader.vue';
+import AppFooter from '@/components/footer/AppFooter.vue'
+import AppHeader from '@/components/header_components/AppHeader.vue'
+import { provide } from 'vue'
+import { RouterView } from 'vue-router'
 
-import { useThemeStore } from '@/stores/current_theme_store';
-const storeTheme =  useThemeStore()
+import { useThemeStore } from '@/stores/current_theme_store'
+
+const storeTheme = useThemeStore()
+
+const databaseUrl = 'https://c6d8-176-59-7-67.ngrok-free.app/v1/'
+provide('databaseUrl', databaseUrl)
 </script>
 
 <template>
