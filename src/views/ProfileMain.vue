0316ce912c438a915fef671f8c973bae5f71466f
ShinyTrunk <maksim19032007@gmail.com>
2024-06-10 05:33:21 +0300

    update ProfileMain.vue

--- src/views/ProfileMain.vue
+++ src/views/ProfileMain.vue
@@ -6,7 +6,7 @@ import RatingContainer from '@/components/profile_page/RatingContainer.vue'
 
 import { useCurrentUserStore } from '@/stores/current_user_store'
 import axios from 'axios'
-import { inject, provide, reactive, ref } from 'vue'
+import { inject, provide, reactive, ref, onBeforeMount } from 'vue'
 import { useRouter } from 'vue-router'
 
 const storeUser = useCurrentUserStore()
@@ -14,6 +14,22 @@ const router = new useRouter()
 const databaseUrl = inject('databaseUrl')
 
 const _rating_list = ref([])
+const _progress_list = ref([])
+const _achievements_list = ref([])
+const _chart_label = ref('')
+const _all_achievements_list = ref([])
+const _data_loaded = ref(false)
+const _limit = ref(10)
+const _dates = ref([])
+const _stats = ref([])
+
+onBeforeMount( () => {
+  _limit.value = 10
+})
+
+provide('_dates', _dates)
+provide('_stats', _stats)
+provide('_data_loaded', _data_loaded)
 const _rating_settings = reactive({
   operations: ['+'],
   difficulty: 10,
@@ -23,17 +39,64 @@ const _rating_settings = reactive({
   correct_answers: 0,
   wrong_answers: 0
 })
-
-async function getUserRating() {
+async function getChartData() {
   try {
+    _data_loaded.value = false
+    _dates.value = []
+    _stats.value = []
+    let difficulty =
+      _rating_settings.levels_count != 0 ? _rating_settings.levels_count : _rating_settings.time
     let params = new URLSearchParams()
     params.append('game_mode', _rating_settings.game_mode)
     params.append('examples_category', _rating_settings.difficulty)
-    params.append('limit', 10)
-    params.append('offset', 0)
+    params.append('difficulty', difficulty)
     for (let operation of _rating_settings.operations) {
       params.append('math_operations', operation)
     }
+    let request = {
+      params: params,
+      headers: {
+        Authorization: 'Bearer ' + storeUser.accessToken,
+        'Content-Type': 'application/json',
+        accept: 'application/json'
+      }
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    const response = await axios
+      .get(databaseUrl + 'chart/me', request)
+      .then(function (response) {
+        console.log(response)
+        if (response.status === 200) {
+          console.log('Chart data received')
+          // console.log(response.data)
+          _progress_list.value = response.data
+          _data_loaded.value = true
+          console.log('заполненный лист прогресса', _progress_list)
+        } else {
+          console.error('Failed to recieve chart data')
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  } catch (error) {
+    console.error('Error during receiveing:', error)
+  }
+}
+async function getUserRating() {
+  try {
+    let difficulty =
+      _rating_settings.levels_count != 0 ? _rating_settings.levels_count : _rating_settings.time
+    let params = new URLSearchParams()
+    params.append('game_mode', _rating_settings.game_mode)
+    params.append('examples_category', _rating_settings.difficulty)
+    params.append('difficulty', difficulty)
+    for (let operation of _rating_settings.operations) {
+      params.append('math_operations', operation)
+    }
+    params.append('limit', _limit.value)
+    params.append('offset', 0)
 
     let request = {
       params: params,
@@ -65,15 +128,85 @@ async function getUserRating() {
   }
 }
 
+async function getUserAchievements() {
+  try {
+    let request = {
+      headers: {
+        Authorization: 'Bearer ' + storeUser.accessToken,
+        'Content-Type': 'application/json',
+        accept: 'application/json'
+      }
+    }
+    // eslint-disable-next-line no-unused-vars
+    const response = await axios
+      .get(databaseUrl + 'achievements/me', request)
+      .then(function (response) {
+        console.log(response)
+        if (response.status === 200) {
+          console.log('Personal achievements received')
+          console.log(response.data)
+          _achievements_list.value = response.data
+        } else {
+          console.error('Failed to recieve personal achievements')
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  } catch (error) {
+    console.error('Error during receiveing:', error)
+  }
+}
+
+async function getAllAchievements() {
+  try {
+    let request = {
+      headers: {
+        Authorization: 'Bearer ' + storeUser.accessToken,
+        'Content-Type': 'application/json',
+        accept: 'application/json'
+      }
+    }
+    // eslint-disable-next-line no-unused-vars
+    const response = await axios
+      .get(databaseUrl + 'achievements', request)
+      .then(function (response) {
+        console.log(response)
+        if (response.status === 200) {
+          console.log('All achievements received')
+          console.log(response.data)
+          _all_achievements_list.value = response.data
+        } else {
+          console.error('Failed to recieve all achievements')
+        }
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  } catch (error) {
+    console.error('Error during receiveing:', error)
+  }
+}
+
 function unLog() {
   storeUser.changeLoggedState()
   storeUser.changeUsername('')
+  storeUser.changeAccessToken('')
   router.push('/')
 }
 
 provide('_rating_settings', _rating_settings)
 provide('_rating_list', _rating_list)
+provide('_progress_list', _progress_list)
+provide('_achievements_list', _achievements_list)
+provide('_all_achievements_list', _all_achievements_list)
 provide('getUserRating', getUserRating)
+provide('getChartData', getChartData)
+provide('_chart_label', _chart_label)
+provide('getUserAchievements', getUserAchievements)
+provide('getAllAchievements', getAllAchievements)
+provide('_limit', _limit)
+provide('unLog', unLog)
 </script>
 
 <template>
@@ -81,7 +214,6 @@ provide('getUserRating', getUserRating)
     <div class="flex container items-center justify-center mx-auto w-5/6 flex-col my-6 space-x-6">
       <PersonalInfo />
       <RatingContainer />
-      <button @click="unLog">ВЫХОД</button>
     </div>
   </main>
 </template>
