8704802827a5104da0ca2947d5d6ac27bbcbc980
ShinyTrunk <maksim19032007@gmail.com>
2024-06-10 05:32:49 +0300

    update AboutUs.vue

---
+++ src/views/AboutUs.vue
@@ -0,0 +1,20 @@
+<script setup>
+//Футер (один для всей страницы)
+</script>
+
+<template>
+  <main class="h-full p-6 my-6 flex flex-col items-center">
+    <div class="container w-5/6 my-0 mx-auto p-6">
+      <div class="flex justify-between">
+        <img src="./img/sergei.img" alt="" class="w-7xs rounded-box" />
+        <div>
+          <p>Сергей Иксанов</p>
+          <span>Разработчик мобильного приложения</span>
+          <span>Ученик физико-математического лицея-интерната.</span>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped></style>
