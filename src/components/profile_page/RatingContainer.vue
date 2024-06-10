722b4140fbd129807bfcdf65143ac199c6978250
ShinyTrunk <maksim19032007@gmail.com>
2024-06-10 05:32:05 +0300

    update RatingContainer.vue

--- src/components/profile_page/RatingContainer.vue
+++ src/components/profile_page/RatingContainer.vue
@@ -1,13 +1,19 @@
 <script setup>
 //Контейнер, содержащий меню настройки таблицы результатов и саму таблицу результатов
 
-import { provide, ref } from 'vue'
+import AchievementsContainer from '@/components/profile_page/AchievmentsContainer.vue'
+import LineChart from '@/components/profile_page/LineChart.vue'
+import { inject } from 'vue'
 import RatingNavigationBar from './RatingNavigationBar.vue'
 import RatingTable from './RatingTable.vue'
 
-const ratingList = ref([])
-provide('ratingList', ratingList)
-
+const _data_loaded = inject('_data_loaded')
+const getUserRating = inject('getUserRating')
+const _limit = inject('_limit')
+function getMoreData() {
+  _limit.value += 10
+  getUserRating()
+}
 // const _game_session = inject('_game_session')
 </script>
 
@@ -17,9 +23,33 @@ provide('ratingList', ratingList)
       <p class="font-bold text-xl text-secondary-content my-2">ВАШ РЕЙТИНГ</p>
     </div>
     <RatingNavigationBar />
-    <RatingTable />
-    <p class="w-1/6 p-2 border border-primary text-center cursor-pointer" @click="getUserRating">
-      Показать ещё
-    </p>
+    <div
+      class="w-full mt-6 lg:mt-0 rounded shadow text-neutral-content border border-primary rounded-box overflow-x-auto h-13xs"
+      v-if="_data_loaded"
+    >
+      <LineChart />
+    </div>
+    <div
+      class="w-full mt-6 lg:mt-0 rounded shadow text-neutral-content border border-primary rounded-box overflow-x-auto h-13xs skeleton"
+      v-else
+    ></div>
+    <div
+      class="w-full mt-6 lg:mt-0 rounded shadow text-neutral-content border border-primary rounded-box overflow-x-auto h-13xs"
+      v-if="_data_loaded"
+      @scrollend="getMoreData()"
+    >
+      <RatingTable />
+    </div>
+    <div
+      class="w-full mt-6 lg:mt-0 rounded shadow text-neutral-content border border-primary rounded-box overflow-x-auto h-13xs skeleton"
+      v-else
+    ></div>
+    <section class="flex flex-col items-center w-full">
+      <p class="text-3xl pr-6 text-center text-base-content my-3">ДОСТИЖЕНИЯ</p>
+      <div class="w-full flex flex-wrap bg-base-300 p-6 gap-4 pl-14 rounded-box justify-center" v-if="_data_loaded">
+        <AchievementsContainer />
+      </div>
+      <div class="w-full flex flex-wrap bg-base-300 p-6 gap-4 pl-14 rounded-box skeleton" v-else></div>
+    </section>
   </section>
 </template>
