a5c66a213167fd00aa8e67a2c1e40eefcde35dec
ShinyTrunk <maksim19032007@gmail.com>
2024-06-10 05:26:27 +0300

    add /rating route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { useCurrentUserStore } from '@/stores/current_user_store'
 import AppMain from '@/views/AppMain.vue'
 import ProfileMain from '@/views/ProfileMain.vue'
 import OverallRatingMain from '@/views/OverallRatingMain.vue'
+import AboutUs from '@/views/AboutUs.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -20,7 +21,8 @@ const router = createRouter({
         }
       }
     },
-    { path: '/rating', component: OverallRatingMain }
+    { path: '/rating', component: OverallRatingMain },
+    { path: '/about-us', component: AboutUs }
   ]
 })
 
