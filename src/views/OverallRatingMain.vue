28f9d96947161fc1e86af4c8f4c82fbbc6a92751
ShinyTrunk <maksim19032007@gmail.com>
2024-06-10 05:33:05 +0300

    update OverallRatingMain.vue

--- src/views/OverallRatingMain.vue
+++ src/views/OverallRatingMain.vue
@@ -2,11 +2,13 @@
 import OverallRatingNavigationBar from '@/components/overall_rating/OverallRatingNavigationBar.vue'
 import OverallRatingTable from '@/components/overall_rating/OverallRatingTable.vue'
 import axios from 'axios'
-import { inject, provide, reactive, ref } from 'vue'
+import { onMounted } from 'vue';
+import { inject, provide, reactive, ref, onBeforeMount } from 'vue'
 
 const databaseUrl = inject('databaseUrl')
 
 const _rating_list = ref([])
+const _all_rating_data_received = ref(false)
 const _rating_settings = reactive({
   operations: ['+'],
   difficulty: 10,
@@ -19,21 +21,33 @@ const _rating_settings = reactive({
 const _limit = ref(10)
 const _offset = ref(0)
 
+
+onMounted(()=>{
+  _limit.value = 10
+})
 provide('_limit', _limit)
 provide('_offset', _offset)
 provide('_rating_list', _rating_list)
 provide('_rating_settings', _rating_settings)
 
+
 async function getOverallRating() {
+  _limit.value += 10
+  // console.log(_rating_settings)
+  _all_rating_data_received.value = false
   try {
+    let difficulty =
+      _rating_settings.levels_count != 0 ? _rating_settings.levels_count : _rating_settings.time
     let params = new URLSearchParams()
     params.append('game_mode', _rating_settings.game_mode)
     params.append('examples_category', _rating_settings.difficulty)
+    params.append('difficulty', difficulty)
     params.append('limit', _limit.value)
     params.append('offset', _offset.value)
     for (let operation of _rating_settings.operations) {
       params.append('math_operations', operation)
     }
+    console.log(params)
 
     let request = {
       params: params
@@ -46,8 +60,9 @@ async function getOverallRating() {
         console.log(response)
         if (response.status === 200) {
           console.log('Data received')
-          console.log(response.data)
+          console.log(response)
           _rating_list.value = response.data
+          _all_rating_data_received.value = true
         } else {
           console.error('Data failed')
         }
@@ -60,6 +75,10 @@ async function getOverallRating() {
   }
 }
 
+onBeforeMount(async () => {
+  await getOverallRating()
+})
+
 provide('getOverallRating', getOverallRating)
 </script>
 
@@ -67,7 +86,19 @@ provide('getOverallRating', getOverallRating)
   <main class="h-full p-6 my-6 flex flex-col items-center">
     <div class="flex container items-center justify-center mx-auto w-5/6 flex-col my-6 space-x-6">
       <OverallRatingNavigationBar />
-      <OverallRatingTable />
+      <div class="w-full my-6">
+        <div
+          class="w-full mt-6 lg:mt-0 rounded shadow text-neutral-content border border-primary rounded-box overflow-x-auto h-13xs"
+          v-if="_all_rating_data_received"
+          @scrollend="getOverallRating()"
+        >
+          <OverallRatingTable />
+        </div>
+        <div
+          class="w-full mt-6 lg:mt-0 rounded shadow text-neutral-content border border-primary rounded-box overflow-x-auto h-13xs skeleton"
+          v-else
+        ></div>
+      </div>
     </div>
   </main>
 </template>
